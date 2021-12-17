c869478d313e113896f5d62555239782be76f012
tanthao <tanthao2001@gmail.com>
2021-12-17 15:46:38 +0700

    layout project

---
+++ app/src/main/java/com/example/doannt118/MainActivityProject.java
@@ -0,0 +1,50 @@
+package com.example.doannt118;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityProject extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.project_activity);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Project");
+        //String title = actionBar.getTitle().toString();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_project, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.menu1:
+                //code xử lý khi bấm menu1
+                break;
+            case R.id.menu2:
+                //code xử lý khi bấm menu2
+                break;
+            case R.id.menu3:
+                //code xử lý khi bấm menu3
+                break;
+            default:break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
