8cccdb4406d93405f163a7f2b02974f78ba61914
hauphanlvc <hauphanlvc@gmail.com>
2021-12-19 03:04:42 +0700

    thêm nút add task

--- app/src/main/java/com/example/doannt118/MainActivity.java
+++ app/src/main/java/com/example/doannt118/MainActivity.java
@@ -7,7 +7,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout; // layout chính của board
     NavigationView navigationView;
 
+    FloatingActionButton fabAddTask,fabAdd;
+    TextView tvAddCard;
+    // kiểm tra nút add có đang nhấn hay không
+    Boolean isAllFabsVisible;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -38,6 +46,46 @@ public class MainActivity extends AppCompatActivity {
                 view.getContext().startActivity(intent);
             }
         });
+
+        AddTaskButton();
+
+    }
+    public void AddTaskButton()
+    {
+        isAllFabsVisible = false;
+
+        fabAdd = (FloatingActionButton) findViewById(R.id.fab);
+        fabAddTask = (FloatingActionButton) findViewById(R.id.fabAddTask);
+        tvAddCard = (TextView) findViewById(R.id.textViewAddTask);
+        fabAdd.show();
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isAllFabsVisible)
+                {
+                    fabAddTask.show();
+                    tvAddCard.setVisibility(View.VISIBLE);
+                    fabAdd.show();
+                    isAllFabsVisible = true;
+                }
+                else
+                {
+                    fabAddTask.hide();
+                    tvAddCard.setVisibility(View.GONE);
+                    fabAdd.show();
+                    isAllFabsVisible= false;
+
+                }
+                fabAddTask.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "thêm task nào ", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
+
 
 
     }
