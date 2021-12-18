99f8973a56b25c12aaba1ae516e5ce8770ee79d0
hauphanlvc <hauphanlvc@gmail.com>
2021-12-18 19:32:45 +0700

    sửa lại boards và thêm navigation view

--- app/src/main/java/com/example/doannt118/MainActivity.java
+++ app/src/main/java/com/example/doannt118/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.doannt118;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout; // layout chính của board
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_board);
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        getSupportActionBar().setDisplayShowHomeEnabled(false);
+//        getActionBar().setDisplayHomeAsUpEnabled(false);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+//        navigationView = findViewById(R.id.design_navigation_view);
+//        navigationView.setItemIconTintList(null);
     }
 }
