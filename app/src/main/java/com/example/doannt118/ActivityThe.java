31fa0373be319e88db9f9d04be7f19e4e7976fed
HaiNguyen2404 <april24th.red@gmail.com>
2021-12-21 00:24:15 +0700

    sửa date picker

--- app/src/main/java/com/example/doannt118/ActivityThe.java
+++ app/src/main/java/com/example/doannt118/ActivityThe.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -17,6 +18,7 @@ import java.util.Calendar;
 public class ActivityThe extends AppCompatActivity {
 
     EditText etDate;
+    ImageView ivCalendar;
     DatePickerDialog.OnDateSetListener setListener;
 
     @Override
@@ -25,13 +27,14 @@ public class ActivityThe extends AppCompatActivity {
         setContentView(R.layout.card_layout);
 
         etDate = findViewById(R.id.etDeadline);
+        ivCalendar = findViewById(R.id.ivCalendar);
 
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        etDate.setOnClickListener(new View.OnClickListener() {
+        ivCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(ActivityThe.this, new DatePickerDialog.OnDateSetListener() {
