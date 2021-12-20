c2c15954616cf0d036ff75c0ff58099c543d0a92
HaiNguyen2404 <april24th.red@gmail.com>
2021-12-20 17:46:02 +0700

    sửa layout thẻ

---
+++ app/src/main/java/com/example/doannt118/ActivityThe.java
@@ -0,0 +1,13 @@
+package com.example.doannt118;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityThe extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_layout);
+    }
+}
