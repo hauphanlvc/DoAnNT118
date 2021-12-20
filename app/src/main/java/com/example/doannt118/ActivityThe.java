7111b98e833f33335f6d3fdcbff95b68cb87af82
HaiNguyen2404 <april24th.red@gmail.com>
2021-12-20 18:42:42 +0700

    sửa layout thẻ

--- app/src/main/java/com/example/doannt118/ActivityThe.java
+++ app/src/main/java/com/example/doannt118/ActivityThe.java
@@ -1,7 +1,10 @@
 package com.example.doannt118;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityThe extends AppCompatActivity {
@@ -9,5 +12,18 @@ public class ActivityThe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_layout);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
