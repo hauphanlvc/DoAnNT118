88e3fc7af3d6a775478e086fad2073fdb24819c9
tanthao <tanthao2001@gmail.com>
2022-01-13 08:28:29 +0700

    sua lai layout

--- app/src/main/java/com/example/doannt118/MainActivityProject.java
+++ app/src/main/java/com/example/doannt118/MainActivityProject.java
@@ -14,37 +14,34 @@ public class MainActivityProject extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.project_activity);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Danh sách");
-        //String title = actionBar.getTitle().toString();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
 
     }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_project, menu);
-//        return true;
-//    }
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId())
-//        {
-//            case android.R.id.home:
-//                onBackPressed();
-//                return true;
-//            case R.id.menu1:
-//                //code xử lý khi bấm menu1
-//                break;
-//            case R.id.menu2:
-//                //code xử lý khi bấm menu2
-//                break;
-//            case R.id.menu3:
-//                //code xử lý khi bấm menu3
-//                break;
-//            default:break;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+  /*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_project, menu);
+        return true;
+   }
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+   switch (item.getItemId())
+       {
+           case android.R.id.home:
+               onBackPressed();
+               return true;
+            case R.id.menu1:
+               //code xử lý khi bấm menu1
+             break;
+            case R.id.menu2:
+                //code xử lý khi bấm menu2
+                break;
+           case R.id.menu3:
+               //code xử lý khi bấm menu3
+               break;
+           default:break;
+       }
+
+       return super.onOptionsItemSelected(item);
+    }*/
 }
